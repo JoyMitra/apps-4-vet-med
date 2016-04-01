7e661624596e5d06072502cf8ba3398fee307e63
JoyMitra <joymitro1989@gmail.com>
2016-03-31 23:18:50 -0500

    changed directory structure

---
+++ bse-a4vm-android/app/src/main/java/com/cis/ksu/ezcount/Collections.java
@@ -0,0 +1,139 @@
+package com.cis.ksu.ezcount;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.ArrayAdapter;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.AdapterView;
+import android.content.Intent;
+import android.widget.Button;
+import android.view.View;
+import android.widget.Toast;
+
+
+import com.cis.ksu.ezcount.Constants.Constant;
+import com.cis.ksu.ezcount.util.SharedPrefUtil;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+import static android.R.layout.simple_list_item_1;
+
+
+public class Collections extends AppCompatActivity{
+
+    /*private static final String[] items = {"Date:RanchName1","Date:RanchName2",
+            "Date:RanchName3"};*/
+
+
+
+    private Button btn = null;
+    private ListView lv = null;
+    private Set<String> ranchNames = null;
+    private Set<String> orderedItems = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_collections);
+
+
+
+
+        btn = (Button) findViewById(R.id.addGrpBtn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final HashSet<String> keySet = (HashSet<String>) SharedPrefUtil.getValue(
+                        getApplicationContext(), Constant.PREFS_GROUP_INFO, Constant.KEY_GROUP);
+                String key = Util.getKey(keySet);
+                Intent goToNewGroup = new Intent(getApplicationContext(), NewGroup.class);
+                goToNewGroup.putExtra("grpKey", key);
+                startActivity(goToNewGroup);
+            }
+        });
+
+        lv = (ListView) findViewById(R.id.ranchId);
+
+
+
+
+    }
+
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+        //loading data from group
+        final Set<String> items = SharedPrefUtil.getValue(getApplicationContext(), Constant.PREFS_GROUP_INFO,Constant.KEY_GROUP);
+        if(items != null)
+        {
+            ranchNames = new LinkedHashSet<>();
+            orderedItems = new LinkedHashSet<>();
+            for(String item : items)
+            {
+                Set<String> groups = null;
+                groups = SharedPrefUtil.getValue(getApplicationContext(),Constant.PREFS_GROUP_INFO,item);
+                if(groups != null)
+                {
+                    String name = null;
+                    String datetime = null;
+                    for(String group : groups)
+                    {
+                        String[] entry = group.split("=");
+                        if(entry != null && entry.length==2 && entry[0].equalsIgnoreCase("Ranch Name"))
+                        {
+                            name = entry[1];
+                            /*Date cDate = new Date();
+                            String fDate = new SimpleDateFormat("dd-MM-yyyy").format(cDate);
+                            ranchNames.add(fDate + ":" + entry[1]);
+                            break;*/
+                        }
+                        if(entry != null && entry.length==2 && entry[0].equalsIgnoreCase("TimeStamp"))
+                        {
+                            datetime = entry[1];
+                        }
+                        if(name!=null && datetime!=null)
+                        {
+                            ranchNames.add(datetime + ":" + name);
+                            break;
+                        }
+
+                    }
+                    orderedItems.add(item);
+                }
+
+            }
+        }
+
+        if(ranchNames!=null)
+        {
+            ArrayAdapter<Object> dataAdapter = new ArrayAdapter<Object>(this, simple_list_item_1,ranchNames.toArray());
+            lv.setAdapter(dataAdapter);
+        }
+
+        lv.setOnItemClickListener(new OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String selectedString = null;
+                if (lv.getItemAtPosition(position) != null) {
+                    selectedString = lv.getItemAtPosition(position).toString();
+                }
+                Intent goToEditColls = new Intent(getApplicationContext(), EditCollections.class);
+                Bundle extras = new Bundle();
+                extras.putString("ranch", selectedString);
+                extras.putString("GrpId", orderedItems.toArray()[position].toString());
+                goToEditColls.putExtras(extras);
+                startActivity(goToEditColls);
+
+            }
+        });
+    }
+}
+
---
+++ bse-a4vm-android/app/src/main/java/com/cis/ksu/ezcount/Constants/Constant.java
@@ -0,0 +1,142 @@
+package com.cis.ksu.ezcount.Constants;
+
+/**
+ * Created by Joy on 2/22/16.
+ */
+public class Constant {
+    public static String PREFS_FILE_VET_INFO = "vet_info_file";
+    public static String PREFS_FILE_MORPH_INFO = "morphology_settings_file";
+    public static String PREFS_GROUP_INFO = "group_info_file";
+    public static String PREFS_BULL_INFO = "bull_info_file";
+    public static String PREFS_MATING_INFO = "mating_info_file";
+    public static String PREFS_PHY_PRAMS_INFO = "phy_params_file";
+    public static String PREFS_BULL_MEASUREMENT_INFO = "bull_measurement_file";
+    public static String PREFS_BULL_MORPHOLOGY_INFO = "bull_morphology_file";
+    public static String PREFS_MORPHOLOGY_COUNT_KEYS = "morphology_count_keys_file";
+    public static String PREFS_MOTILITY_INFO = "motility_info_file";
+    public static String PREFS_CLASSIFICATION_INFO = "classify_info_file";
+    public static String PREFS_COMMENTS_INFO = "comments_info_file";
+    public static String KEY_VET = "vetinfoKey";
+    public static String KEY_MORPHOLOGY = "morphologyKeys";
+    public static String KEY_GROUP = "groupKeys";
+    public static String KEY_BULL = "bullKeys";
+    public static String KEY_MORPHOLOGY_COUNT_KEY = "morphCountKey";
+
+    //id hints
+    public static String BULL_INFO_ID_1 = "Tag";
+    public static String BULL_INFO_ID_2 = "Tattoo";
+    public static String BULL_INFO_ID_3 = "RFID";
+    public static String BULL_INFO_ID_4 = "Brand";
+
+    //Bull Info prefix
+    public static String BULL_INFO_PREFIX = "Bull ID";
+
+    //Messages
+    public static String MESSAGE_TOTAL = "Total Count : ";
+    public static String DATE_MSG = "Date Collected : ";
+        public static String CSV_MSG = "CSV file Status";
+    //CSV headings
+    public static String CSV_HEADING = "animal_age," +
+            "animal_ageType," +
+            "animal_brand," +
+            "animal_breed," +
+            "animal_dateOfBirth," +
+            "animal_lotNumber," +
+            "animal_rfid," +
+            "animal_tag," +
+            "animal_tattoo," +
+            "animal_other," +
+            "bodyPartInfo_bodyParts_0_description," +
+            "bodyPartInfo_bodyParts_0_name," +
+            "bodyPartInfo_bodyParts_0_normal," +
+            "bodyPartInfo_bodyParts_10_description," +
+            "bodyPartInfo_bodyParts_10_name," +
+            "bodyPartInfo_bodyParts_10_normal," +
+            "bodyPartInfo_bodyParts_1_description," +
+            "bodyPartInfo_bodyParts_1_name," +
+            "bodyPartInfo_bodyParts_1_normal," +
+            "bodyPartInfo_bodyParts_2_description," +
+            "bodyPartInfo_bodyParts_2_name," +
+            "bodyPartInfo_bodyParts_2_normal," +
+            "bodyPartInfo_bodyParts_3_description," +
+            "bodyPartInfo_bodyParts_3_name," +
+            "bodyPartInfo_bodyParts_3_normal," +
+            "bodyPartInfo_bodyParts_4_description," +
+            "bodyPartInfo_bodyParts_4_name," +
+            "bodyPartInfo_bodyParts_4_normal," +
+            "bodyPartInfo_bodyParts_5_description," +
+            "bodyPartInfo_bodyParts_5_name," +
+            "bodyPartInfo_bodyParts_5_normal," +
+            "bodyPartInfo_bodyParts_6_description," +
+            "bodyPartInfo_bodyParts_6_name," +
+            "bodyPartInfo_bodyParts_6_normal," +
+            "bodyPartInfo_bodyParts_7_description," +
+            "bodyPartInfo_bodyParts_7_name," +
+            "bodyPartInfo_bodyParts_7_normal," +
+            "bodyPartInfo_bodyParts_8_description," +
+            "bodyPartInfo_bodyParts_8_name," +
+            "bodyPartInfo_bodyParts_8_normal," +
+            "bodyPartInfo_bodyParts_9_description," +
+            "bodyPartInfo_bodyParts_9_name," +
+            "bodyPartInfo_bodyParts_9_normal," +
+            "collectionNumber," +
+            "completed," +
+            "datePerformed," +
+            "generalInfo_classification," +
+            "generalInfo_comments," +
+            "matingInfo_comments," +
+            "matingInfo_performance," +
+            "matingInfo_performanceDescription," +
+            "matingInfo_seasonsUsed," +
+            "matingInfo_sirePastureType," +
+            "morphologyInfo_comments," +
+            "morphologyInfo_morphologyMeasurements_0_count," +
+            "morphologyInfo_morphologyMeasurements_0_morphologyChoice_label," +
+            "morphologyInfo_morphologyMeasurements_1_count," +
+            "morphologyInfo_morphologyMeasurements_1_morphologyChoice_label," +
+            "morphologyInfo_morphologyMeasurements_2_count," +
+            "morphologyInfo_morphologyMeasurements_2_morphologyChoice_label," +
+            "morphologyInfo_morphologyMeasurements_3_count," +
+            "morphologyInfo_morphologyMeasurements_3_morphologyChoice_label," +
+            "morphologyInfo_morphologyMeasurements_4_count," +
+            "morphologyInfo_morphologyMeasurements_4_morphologyChoice_label," +
+            "morphologyInfo_morphologyMeasurements_5_count," +
+            "morphologyInfo_morphologyMeasurements_5_morphologyChoice_label," +
+            "morphologyInfo_morphologyMeasurements_6_count," +
+            "morphologyInfo_morphologyMeasurements_6_morphologyChoice_label," +
+            "morphologyInfo_morphologyMeasurements_7_count," +
+            "morphologyInfo_morphologyMeasurements_7_morphologyChoice_label," +
+            "motilityInfo_comments," +
+            "motilityInfo_grossMotilityCategory," +
+            "motilityInfo_individualMotility," +
+            "motilityInfo_motilityPercent," +
+            "physicalInfo_bodyCondition," +
+            "physicalInfo_comments," +
+            "physicalInfo_frameScore," +
+            "physicalInfo_hipHeight," +
+            "physicalInfo_hipHeightUnits," +
+            "physicalInfo_pelvicXMeasure," +
+            "physicalInfo_pelvicYMeasure," +
+            "physicalInfo_scrotalCircumference," +
+            "rancher_address_address1," +
+            "rancher_address_address2," +
+            "rancher_address_city," +
+            "rancher_address_email," +
+            "rancher_address_phone," +
+            "rancher_address_state," +
+            "rancher_address_zip," +
+            "rancher_firstName," +
+            "rancher_lastName," +
+            "rancher_ranchName," +
+            "uuid," +
+            "veterinarian_address_address1," +
+            "veterinarian_address_address2," +
+            "veterinarian_address_city," +
+            "veterinarian_address_email," +
+            "veterinarian_address_phone," +
+            "veterinarian_address_state," +
+            "veterinarian_address_zip," +
+            "veterinarian_clinicName," +
+            "veterinarian_firstName," +
+            "veterinarian_lastName";
+}
