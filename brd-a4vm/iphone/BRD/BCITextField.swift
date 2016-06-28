90a3427e2799ffca87f1526977e1b0d897093ccd
kevb10 <kevinmanase@k-state.edu>
2016-06-28 12:51:31 -0500

    added constraints

---
+++ brd-a4vm/iphone/BRD/BCITextField.swift
@@ -0,0 +1,21 @@
+//
+//  BCITextField.swift
+//  BRD
+//
+//  Created by Kevin Manase on 6/28/16.
+//  Copyright © 2016 Beef Cattle Institute. All rights reserved.
+//
+
+import UIKit
+
+class BCITextField: UITextField {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
