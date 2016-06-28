6b21151557e03ed6c8f918ec6fd4b1ca2501f4a8
kevb10 <kevinmanase@k-state.edu>
2016-06-28 11:38:10 -0500

    allow scrolling resolve #102

--- brd-a4vm/iphone/BRD/BCIAppDelegate.swift
+++ brd-a4vm/iphone/BRD/BCIAppDelegate.swift
@@ -8,7 +8,6 @@
 
 import UIKit
 import CoreData
-import IQKeyboardManagerSwift
 
 @UIApplicationMain
 class BCIAppDelegate: UIResponder, UIApplicationDelegate {
@@ -19,7 +18,6 @@ class BCIAppDelegate: UIResponder, UIApplicationDelegate {
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
         
-        IQKeyboardManager.sharedManager().enable = true
         
         return true
     }
