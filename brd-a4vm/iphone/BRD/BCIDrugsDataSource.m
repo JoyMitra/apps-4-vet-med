2d17984e4be8d99cf00d081a930b5775bb57984f
kevb10 <kevinmanase@k-state.edu>
2016-07-26 23:49:27 -0500

    put back a necessary function

--- brd-a4vm/iphone/BRD/BCIDrugsDataSource.m
+++ brd-a4vm/iphone/BRD/BCIDrugsDataSource.m
@@ -23,6 +23,28 @@
 #pragma mark - MLPAutoCompleteTextField DataSource
 
 
+//example of asynchronous fetch:
+- (void)autoCompleteTextField:(MLPAutoCompleteTextField *)textField
+ possibleCompletionsForString:(NSString *)string
+            completionHandler:(void (^)(NSArray *))handler {
+    dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0);
+    dispatch_async(queue, ^{
+        if(self.simulateLatency){
+            CGFloat seconds = arc4random_uniform(4)+arc4random_uniform(4); //normal distribution
+            NSLog(@"sleeping fetch of completions for %f", seconds);
+            sleep(seconds);
+        }
+
+        NSArray *completions;
+        if(self.testWithAutoCompleteObjectsInsteadOfStrings){
+            completions = [self allDrugObjects];
+        } else {
+            completions = [self allDrugs];
+        }
+        handler(completions);
+    });
+}
+
 // Returns a list of text completion
 - (NSArray *)allDrugObjects
 {
