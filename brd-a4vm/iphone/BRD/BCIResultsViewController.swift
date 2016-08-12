a6d6c15f4fc03513cb084c8f4b45ba178f1fa786
kevb10 <kevinmanase@k-state.edu>
2016-08-12 09:27:32 -0500

    added tables to show cost breakdown

--- brd-a4vm/iphone/BRD/BCIResultsViewController.swift
+++ brd-a4vm/iphone/BRD/BCIResultsViewController.swift
@@ -8,15 +8,40 @@
 
 import UIKit
 
-class BCIResultsViewController: UIViewController {
+enum Drugs {
+    case Drug1
+    case Drug2
+    case None
+}
+
+class BCIResultsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     let source = BCIDataSource.sharedInstance
     @IBOutlet weak var resultsTextView: UITextView!
     let doubleFormat = ".2"
+    
+    
+    let labelsDictionary = ["drugname":"",
+                       "tfp":"Tx Failure Percent",
+                       "cfr":"Case Fatality Rate",
+                       "cot":"Cost of Treatment",
+                       "chronic":"Chronic Percentage"]
+    var drug1Dictionary: [String:String] = [:]
+    var drug2Dictionary: [String:String] = [:]
+    var keys: [String] = ["drugname","tfp","cfr","cot","chronic"]
 
+    @IBOutlet weak var differentialTextView: UITextView!
+    @IBOutlet weak var drug1TableView: UITableView!
+    @IBOutlet weak var drug2TableView: UITableView!
+    @IBOutlet weak var labelsTableView: UITableView!
+    let drug1CellIdentifier = "drug1"
+    let drug2CellIdentifier = "drug2"
+    let labelsCellIdentifier = "labels"
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        prepDictionary()
         
         // Use the formulas provided
         source.calculateEverything(test: false)
@@ -40,7 +65,7 @@ class BCIResultsViewController: UIViewController {
         // The text displayed at the results screen
         resultString = "Drug \(bestDrugName) shows $ \(differenceInReturnString)/head advantage over Drug \(otherDrugName) using the information provided."
         resultsTextView.text = resultString
-        
+                
     }
 
     override func viewDidDisappear(animated: Bool) {
@@ -61,6 +86,60 @@ class BCIResultsViewController: UIViewController {
 
         
         self.navigationController?.popToRootViewControllerAnimated(true)
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return keys.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        var cell:UITableViewCell?
+        let key = keys[indexPath.row]
+        if (tableView == self.labelsTableView) {
+            cell = self.labelsTableView.dequeueReusableCellWithIdentifier(labelsCellIdentifier)
+            cell?.textLabel?.text = labelsDictionary[key]
+        } else if (tableView == self.drug1TableView) {
+            cell = self.drug1TableView.dequeueReusableCellWithIdentifier(drug1CellIdentifier)
+            cell?.textLabel?.text = drug1Dictionary[key]
+            if (indexPath.row > 0 && betterDrugAtIndex(key) == Drugs.Drug1) {
+                cell?.backgroundColor = .greenColor()
+            }
+        } else {
+            cell = self.drug2TableView.dequeueReusableCellWithIdentifier(drug2CellIdentifier)
+            cell?.textLabel?.text = drug2Dictionary[key]
+            if (indexPath.row > 0 && betterDrugAtIndex(key) == Drugs.Drug2) {
+                cell?.backgroundColor = .greenColor()
+            }
+        }
+        cell?.selectionStyle = .None
+        
+        return cell ?? UITableViewCell()
+    }
+    
+    func betterDrugAtIndex(key: String) -> Drugs {
+        let drug1 = drug1Dictionary[key]
+        let drug2 = drug2Dictionary[key]
+
+        if (key == "drugname" || drug1 == nil || drug2 == nil) {
+            return Drugs.None
+        } else if (Double(drug1!) < Double(drug2!)) {
+            return Drugs.Drug1
+        }
+        return Drugs.Drug2
+    }
+    
+    func prepDictionary() {
+        drug1Dictionary = ["drugname":source.drug1Name,
+                      "tfp":String(source.tfpa.format(doubleFormat)),
+                      "cfr":String(source.cfra.format(doubleFormat)),
+                      "cot":String(source.cta1.format(doubleFormat)),
+                      "chronic":String(source.cpa.format(doubleFormat))]
+        drug2Dictionary = ["drugname":source.drug2Name,
+                      "tfp":String(source.tfpb.format(doubleFormat)),
+                      "cfr":String(source.cfrb.format(doubleFormat)),
+                      "cot":String(source.ctb1.format(doubleFormat)),
+                      "chronic":String(source.cpb.format(doubleFormat))]
     }
 
     /*
